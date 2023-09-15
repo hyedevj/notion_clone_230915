8d506dcf6256e4adab002c2bcab5fccb23175f62
hyedevj <127413734+hyedevj@users.noreply.github.com>
2023-09-15 21:38:43 +0900

    테스트 데이터 출력

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import Editor from "./Editor.js";
 import Sidebar from "./Sidebar.js";
 
-export default function App({ $target }) {
-  new Sidebar({ $target });
-  new Editor({ $target });
+export default function App ({ $target }) {
+    const initialState = []
+    new Sidebar({ $target, initialState })
+    new Editor({ $target })
 }
