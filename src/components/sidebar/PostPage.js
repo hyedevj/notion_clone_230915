c9b6c9db3d753bbde4bd994ccbba26ee4e0549d5
hyedevj <hyedevj@gmail.com>
2023-09-27 15:35:08 +0900

    New button 만들기

--- src/components/sidebar/PostPage.js
+++ src/components/sidebar/PostPage.js
@@ -34,6 +34,12 @@ export default function PostPage({ $target }) {
     $target: $page,
     testData
   })
+
+  // TODO: 컴포넌트화 시키기
+  const $newBtn = document.createElement("button")
+  $newBtn.textContent = "+ New Page"
+  $newBtn.className = "addNew"
+  $page.appendChild($newBtn)
   
   //this.state = testData
 
