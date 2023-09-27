5d246d36334d28da7b086896c79804c6886a76d2
hyedevj <hyedevj@gmail.com>
2023-09-27 14:36:34 +0900

    초기 스타일 작업

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,21 @@
-import Editor from "./Editor.js";
-import Sidebar from "./Sidebar.js";
+import Editor from "./editor/Editor.js";
+import PostPage from "./sidebar/PostPage.js";
 
-export default function App ({ $target }) {
+export default function App({ $target }) {
+    const $listContainer = document.createElement('div')
+    $listContainer.className = 'listContainer'
+    const $rendingContainer = document.createElement('div')
+    $rendingContainer.className = 'rendingContainer'
+
+    $target.appendChild($listContainer)
+    $target.appendChild($rendingContainer)
+
     const initialState = []
-    new Sidebar({ $target, initialState })
-    new Editor({ $target })
+    new PostPage({
+        $target: $listContainer,
+        initialState
+    })
+    new Editor({
+        $target: $rendingContainer
+    })
 }
---
+++ src/components/sidebar/PostPage.js
@@ -0,0 +1,38 @@
+import PostList from "./PostList.js"
+
+export default function PostPage({ $target, initialState }) {
+  const $page = document.createElement("div")
+  $page.className = 'documentDiv'
+  $target.appendChild($page)
+  new PostList({
+    $target: $page
+  })
+
+  const testData = [
+    {
+      "id": 1, // Document id
+      "title": "노션을 만들자", // Document title
+      "documents": [
+        {
+          "id": 2,
+          "title": "블라블라",
+          "documents": [
+            {
+              "id": 3,
+              "title": "함냐함냐",
+              "documents": []
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "id": 4,
+      "title": "hello!",
+      "documents": []
+    }
+  ]
+
+  this.state = testData
+
+}
